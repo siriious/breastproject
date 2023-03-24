32d2e39fbf171d1889a44fa8223a7d48206b4412
siriious <127997381+siriious@users.noreply.github.com>
2023-03-24 23:54:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+import pickle
+
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+scaler=pickle.load(open('scale.pkl','rb'))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    scale_data=scaler.transform([data])
+    result=model.predict(scale_data)
+
+    final_result=result[0]
+    
+    if final_result=="M":
+        return "<h1 style='color:green'>The Female is Suffering From = Malignant Type of Breast Cancer</h1>"
+        
+    else:
+        return "<h1 style='color:red'>The Female is Suffering From = Benign Type of Breast Cancer</h1>"
+    
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+    # "<h1 style='color:red'>The Predicted Variety is = Versicolor</h1>"
